93b39feedcfdc9eed81cf8ca5fb303eb9c8af1ec
somto <somto565@gmail.com>
2023-12-07 11:05:59 +0100

    done with routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,33 @@ import { useState } from 'react'
 
 import './App.css'
 import Home from './Pages/Home/Home'
-
+import Login from './Pages/Login/Login'
+import List from './Pages/List/List'
+import Single from './Pages/Single/Single' 
+import New from './Pages/New/New'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 function App() {
 
 
   return (
-    <>
-      <Home />
-    </>
+   <Router>
+    <Routes>
+      <Route path="/"> 
+        <Route index element={<Home />} />
+        <Route path='login' element={<Login />} />
+        <Route path='users'>
+          <Route index element={<List />}/>
+          <Route path=':userId' element={<Single />}/>
+          <Route path='new' element={<New />}/>
+        </Route>
+        <Route path='products'>
+          <Route index element={<List />}/>
+          <Route path=':productId' element={<Single />}/>
+          <Route path='new' element={<New />}/>
+        </Route>
+      </Route>
+    </Routes>
+   </Router>
   )
 }
 
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,8 @@
-import React from 'react'
 import "./home.scss"
 
 const Home = () => {
   return (
-    <div className='home'>
-        <div className="title">
-            Chrstphr
-        </div>
-    </div>
+    
   )
 }
 
