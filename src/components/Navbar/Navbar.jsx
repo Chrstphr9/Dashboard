69436dda58bf639f4f5d21e2c5ca594da12bf27c
somto <somto565@gmail.com>
2023-12-08 09:33:01 +0100

    Added search bar and language icon to the navbar.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,20 @@
 import './Navbar.scss'
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="wrapper">
-        Navbar
+        <div className="search">
+          <input type="text" placeholder='Search...' />
+          <SearchOutlinedIcon />
+        </div>
+        <div className="items">
+          <div className="item">
+            
+          </div>
+        </div>
       </div>
     </div>
   )
