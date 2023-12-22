ff1f743889f40c9fb73a129aa83a7d23a2ddfc84
somto <somto565@gmail.com>
2023-12-22 08:22:39 +0100

    Add chart and summary section to the Featured component

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -2,6 +2,8 @@ import MoreVerticon  from '@mui/icons-material/MoreVert'
 import './Featured.scss'
 import { CircularProgressbar } from 'react-circular-progressbar'
 import "react-circular-progressbar/dist/styles.css/"
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
 
 const Featured = () => {
   return (
@@ -18,7 +20,27 @@ const Featured = () => {
         </div>
         <p className="title">Total Sales Made Today</p>
         <p className="amount">$420</p>
-        <p className="title">Previous Transaction Processing. Last Payment May Mot Be Included.</p>
+        <p className="desc">Previous Transaction Processing. Last Payment May Mot Be Included.</p>
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle"> Target</div>
+            <div className="itemResult negative">
+                <KeyboardArrowDownIcon fontSize='small' />
+            <div className="resultAmount">$12.4k</div></div>
+          </div>
+          <div className="item">
+            <div className="itemTitle"> Last Week</div>
+            <div className="itemResult positive">
+                <KeyboardArrowUpOutlinedIcon  fontSize='small' />
+            <div className="resultAmount">$12.4k</div></div>
+          </div>
+          <div className="item">
+            <div className="itemTitle"> Last Month</div>
+            <div className="itemResult positive">
+                <KeyboardArrowUpOutlinedIcon fontSize='small' />
+            <div className="resultAmount">$12.4k</div></div>
+          </div>
+        </div>
       </div>
     </div>
   )
