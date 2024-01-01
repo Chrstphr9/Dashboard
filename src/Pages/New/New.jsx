f1631edf62429ca29c7776e8317f0db0445abaf6
somto <somto565@gmail.com>
2024-01-01 10:35:29 +0100

    Add New User page

--- src/Pages/New/New.jsx
+++ src/Pages/New/New.jsx
@@ -1,8 +1,25 @@
 import "./New.scss"
+import Sidebar from "../../components/Sidebar/Sidebar"
+import Navbar from "../../components/Navbar/Navbar"
+
 
 const New = () => {
   return (
-    <div>New</div>
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+       <Navbar />
+        <div className="top">
+        <h1>Add New User</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg" alt="" />
+          </div>
+          <div className="right"></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
