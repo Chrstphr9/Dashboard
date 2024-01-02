f5fe0bcd14bfe62f46cad8ff0de33b4ce497951f
somto <somto565@gmail.com>
2024-01-02 11:52:54 +0100

    Update navigation links to use React Router

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -11,80 +11,88 @@ import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSystemDaydreamOutlined";
 import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
-  return (
-    <div className="sidebar">
-        <div className="top">
-            <span className="logo">ChrstphrAdmin</span>
-        </div>
-        <hr />
-        <div className="center">
-            <ul>
-                <p className="titles">MAIN</p>
-                <li>
-                <DashboardIcon className="icon" />
-                    <span>Dashboard</span>
-                </li>
+    
+    return (
+        <div className="sidebar">
+            <div className="top">
+                <Link to="/" style={{textDecoration: "none"}}>
+                <span className="logo">ChrstphrAdmin</span>
+                </Link>
+            </div>
+            <hr />
+            <div className="center">
+                <ul>
+                    <p className="titles">MAIN</p>
+                    <li>
+                        <DashboardIcon className="icon" />
+                        <span>Dashboard</span>
+                    </li>
 
-                <p className="titles">LISTS</p>
-                <li>
-                    <Person2OutlinedIcon className="icon"/>
-                    <span>Users</span>
-                </li>
-                <li>
-                    <StoreIcon className="icon" />
-                    <span>Products</span>
-                </li>
-                <li>
-                    <CreditCardIcon className="icon" />
-                    <span>Orders</span>
-                </li>
-                <li>
-                    <LocalShippingIcon className="icon" />
-                    <span>Dilvery</span>
-                </li>
+                    <p className="titles">LISTS</p>
+                    <Link to="/users" style={{textDecoration: "none"}}>
+                    <li>
+                        <Person2OutlinedIcon className="icon" />
+                        <span>Users</span>
+                    </li>
+                    </Link >
+                    <Link to="/products" style={{textDecoration: "none"}}>
+                    <li>
+                        <StoreIcon className="icon" />
+                        <span>Products</span>
+                    </li>
+                    </Link>
+                    <li>
+                        <CreditCardIcon className="icon" />
+                        <span>Orders</span>
+                    </li>
+                    <li>
+                        <LocalShippingIcon className="icon" />
+                        <span>Dilvery</span>
+                    </li>
 
-                <p className="titles">USEFUL</p>
-                <li>
-                <InsertChartIcon className="icon" />
-                    <span>Stats</span>
-                </li>
-                <li>
-                    <NotificationsNoneIcon className="icon" />
-                    <span>Notifications</span>
-                </li>
+                    <p className="titles">USEFUL</p>
+                    <li>
+                        <InsertChartIcon className="icon" />
+                        <span>Stats</span>
+                    </li>
+                    <li>
+                        <NotificationsNoneIcon className="icon" />
+                        <span>Notifications</span>
+                    </li>
 
-                <p className="titles">SERVICES</p>
-                <li>
-                    <SettingsSystemDaydreamOutlinedIcon className="icon" />
-                    <span>System Health</span>
-                </li>
-                <li>
-                    <PsychologyOutlinedIcon className="icon" />
-                    <span>Logs</span>
-                </li>
-                <li>
-                    <SettingsApplicationsIcon className="icon" />
-                    <span>Settings</span>
-                </li>
-                <p className="titles">USER</p>
-                <li>
-                    <AccountCircleOutlinedIcon className="icon" />
-                    <span>Profile</span>
-                </li>
-                <li>
-                    <ExitToAppIcon className="icon" />
-                    <span>Logout</span>
-                </li>
-            </ul>
+                    <p className="titles">SERVICES</p>
+                    <li>
+                        <SettingsSystemDaydreamOutlinedIcon className="icon" />
+                        <span>System Health</span>
+                    </li>
+                    <li>
+                        <PsychologyOutlinedIcon className="icon" />
+                        <span>Logs</span>
+                    </li>
+                    <li>
+                        <SettingsApplicationsIcon className="icon" />
+                        <span>Settings</span>
+                    </li>
+                    <p className="titles">USER</p>
+                    <li>
+                        <AccountCircleOutlinedIcon className="icon" />
+                        <span>Profile</span>
+                    </li>
+                    <li>
+                        <ExitToAppIcon className="icon" />
+                        <span>Logout</span>
+                    </li>
+                </ul>
+            </div>
+            <div className="bottom">
+                <div className="colorOption"></div>
+                <div className="colorOption"></div>
+            </div>
         </div>
-        <div className="bottom">
-            <div className="colorOption"></div>
-            <div className="colorOption"></div>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Sidebar
